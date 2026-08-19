a77651bcf1f382b0719471076510ed919e47a8c2
deepthijampala <deepthijampala123@gmail.com>
2026-08-19 20:05:26 +0530

    Add email notifications for contact form

---
+++ src/main/java/com/guru/portfolio_backend/EmailService.java
@@ -0,0 +1,39 @@
+package com.guru.portfolio_backend;
+
+import com.guru.portfolio_backend.entity.ContactMessage;
+
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmailService {
+
+    private final JavaMailSender mailSender;
+
+    public EmailService(JavaMailSender mailSender) {
+        this.mailSender = mailSender;
+    }
+
+    public void sendContactEmail(ContactMessage message) {
+
+        SimpleMailMessage email = new SimpleMailMessage();
+
+        email.setTo(System.getenv("MAIL_TO"));
+
+        email.setSubject(
+                "New Portfolio Contact - " + message.getName());
+
+        email.setText(
+                "You received a new message from your portfolio.\n\n" +
+                        "Name: " + message.getName() + "\n" +
+                        "Email: " + message.getEmail() + "\n" +
+                        "Business: " + message.getBusiness() + "\n\n" +
+                        "Message:\n" +
+                        message.getMessage());
+
+        email.setFrom(System.getenv("MAIL_USERNAME"));
+
+        mailSender.send(email);
+    }
+}
--- src/main/java/com/guru/portfolio_backend/controller/ContactController.java
+++ src/main/java/com/guru/portfolio_backend/controller/ContactController.java
@@ -1,5 +1,6 @@
 package com.guru.portfolio_backend.controller;
 
+import com.guru.portfolio_backend.EmailService;
 import com.guru.portfolio_backend.entity.ContactMessage;
 import com.guru.portfolio_backend.repository.ContactRepository;
 
@@ -15,15 +16,27 @@ import org.springframework.web.bind.annotation.RestController;
 public class ContactController {
 
     private final ContactRepository contactRepository;
+    private final EmailService emailService;
 
-    public ContactController(ContactRepository contactRepository) {
+    public ContactController(
+            ContactRepository contactRepository,
+            EmailService emailService) {
+
         this.contactRepository = contactRepository;
+        this.emailService = emailService;
     }
 
     @PostMapping
     public ContactMessage saveMessage(
             @RequestBody ContactMessage message) {
 
-        return contactRepository.save(message);
+        // 1. Save the message to Aiven MySQL
+        ContactMessage savedMessage = contactRepository.save(message);
+
+        // 2. Send the message to your email
+        emailService.sendContactEmail(savedMessage);
+
+        // 3. Return the saved message
+        return savedMessage;
     }
 }
